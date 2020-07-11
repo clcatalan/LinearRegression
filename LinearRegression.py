bf9b83816f7fd55d6dec4439a85732c1a61c7b1b
Carlos Catalan <crlsctalan23@gmail.com>
2020-07-11 21:32:31 +0800

    simpler read csv

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,17 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from pylab import *
+import pandas as pd
 
 
-with open('student-mat.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Columns: {", ".join(row)}')
-            line_count += 1
-print(f'{line_count} lines.')
-
+student_mat = pd.read_csv("student-mat.csv")
+student_por = pd.read_csv("student-por.csv")
+print(student_mat.head(5))
+print(student_por.head(5))
 
 
 
