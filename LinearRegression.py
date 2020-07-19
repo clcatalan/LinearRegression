c36eb72231f10f9797e8dd2c184246faa8d284db
Carlos Catalan <crlsctalan23@gmail.com>
2020-07-19 18:43:26 +0800

    refactor code into functions

--- LinearRegression.py
+++ LinearRegression.py
@@ -4,55 +4,76 @@ import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
 
-"""
-    attributes ["G1", "G2", "G3", "studytime", "failures", "absences"] ~ 78% accuracy
 
-    TODO: Try more or less attributes to increase accuracy
-    TODO: Input student number in console then return its predicted and actual grade
-"""
+print("-------Student Final Grade Predicter-------")
 #read the csv file, then remove the sep in the csv
 data = pd.read_csv("student-mat.csv", sep=';')
 #print(data.head())
 
 
-def read_input():
+#------------function definitions--------
+"""def readInput():
     idx = int(input('Enter Student Number: '))
+    print(len(data))
     if idx >= 0 and idx < len(data):
         return idx
     else:
         print('Invalid Input')
-        return -1
+        return -1"""
+
+def exportBestAndWorstToCsv():
+    compression_opts = dict(method='zip',
+                        archive_name='top-performers.csv')  
+    data.nlargest(20, 'avg').to_csv('top.zip', index=False,
+            compression=compression_opts)  
+    compression_opts = dict(method='zip',
+                            archive_name='bottom10.csv')  
+    data.nsmallest(20, 'avg').to_csv('worst-performers.zip', index=False,
+            compression=compression_opts)  
+
+def cleanData():
+    #change text values into numbers
+    data['address'] = data['address'].map({'U': 1, 'R': 0})
+    data['Pstatus'] = data['Pstatus'].map({'T': 1, 'A': 0})
+    data['romantic'] = data['romantic'].map({'yes': 1, 'no': 0})
+    data['internet'] = data['internet'].map({'yes': 1, 'no': 0})
 
 
-#def clean_data(data):
-   
-stdNo = read_input()
-#clean_data(data)
+def printPredictions():
+    leftAlign = 'Student No'
+    center = 'Predicted Grade'
+    rightAlign = 'Actual Grade'
+
+
+    print(len(predictions))
+    print(len(y_test))
+    print("-----------------------RESULTS-----------------------")
+    print("| Model Accuracy: ",acc)
+    print("|-------")
+    print(f"| {leftAlign:<15}{center:^10}{rightAlign:>15}")
+    for i in range(len(predictions)):
+        print(f"| {i:<15}{predictions[i]:^10}{y_test[i]:>15}")
+    print("-----------------------------------------------------")
+
+#----------start of app----------
+
+#stdNo = readInput()
 avg = [0] * len(data)
 for x, row in data.iterrows():
     avg[x] = (row["G1"] + row["G2"] + row["G3"]) / 3
 
 
 data["avg"] = avg
+exportBestAndWorstToCsv()
+cleanData()
 
-#change text values into numbers
-data['schoolsup'] = data['schoolsup'].map({'yes': 1, 'no': 0})
-data['famsup'] = data['famsup'].map({'yes': 1, 'no': 0})
-data['address'] = data['address'].map({'U': 1, 'R': 0})
-data['Pstatus'] = data['Pstatus'].map({'T': 1, 'A': 0})
-
-#this is to see the attributes are present in the top performing students. If certain values of attributes appear often in them, then it probably means those attributes are relevant
-print(data.nlargest(10, 'avg'))
-
-#address, pstatus, Medu, Dalc, Walc
 
 #trim data into attributes that we feel are relevant to the model
-#attributes = ["G1", "G2", "G3", "studytime", "failures", "absences"] #80% accurate
-attributes = ["G1", "G2", "G3","studytime", "failures", "absences", "schoolsup", "famsup","address", "Pstatus"]
+attributes = ["G1", "G2", "G3", "studytime", "failures", "absences", "romantic", "traveltime", "internet"]
 data = data[attributes]    
-
 predict = "G3"
 #Label (What we're trying to predict)
+X = np.array(data.drop(columns=[predict]))
 y = np.array(data[predict])
 
 #split each data frame into training and testing sets
@@ -68,26 +89,16 @@ X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X,y,
 
 linear = linear_model.LinearRegression()
 
-#find best fit line foor X and y training data
+#find best fit line for X and y training data
 linear.fit(X_train, y_train)
 #get accuracy of model
 acc = linear.score(X_test, y_test)
-print("Model Accuracy: ",acc)
-#coefficient of our 5 variables in y=mx+b
-#print("Coefficient: \n", linear.coef_)
-#print("Intercept: \n", linear.intercept_)
 
 predictions = linear.predict(X_test)
-leftAlign = 'Student No'
-center = 'Predicted Grade'
-rightAlign = 'Actual Grade'
-print(f"{leftAlign:<15}{center:^10}{rightAlign:>15}")
-print(f"{stdNo:<15}{predictions[stdNo]:^10}{y_test[stdNo]:>15}")
+printPredictions()
 
 
-#for x in range(len(predictions)):
-    #to visualize predictions and their inputs: print(model's predicted value, inputs, actual grade)
-    #print("Student",x, ":" ,predictions[x], "-" , y_test[x])
+
 
 
 
